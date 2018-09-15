7812f214b4008fd80e365b248426e3c85b2a7403
hide-PC\hide <ks78857@qd6.so-net.ne.jp>
2018-09-16 01:23:36 +0900

    ハッシュコード生成追加

---
+++ src/Base/Util/Hash.h
@@ -0,0 +1,25 @@
+#pragma once
+//========================================================
+//include
+#include "../Type.h"
+
+
+namespace base {
+namespace util {
+//========================================================
+//FNV-1ÉnÉbÉVÉÖä÷êî
+uint32 FNV1Hash(const void* p_buff, size_t size)
+{
+	const uint32 OFFSET_BASIS = 2166136261;
+	const uint32 FNV_32_PRIME = 16777619;
+
+	const uint8* p	  = static_cast<const uint8*>(p_buff);
+	uint32		 hash = OFFSET_BASIS;
+
+	for (int i = 0; i < size; ++i)
+		hash = (hash * FNV_32_PRIME) ^ (p[i]);
+
+	return hash;
+}
+} //namespace util
+} //namespace base
